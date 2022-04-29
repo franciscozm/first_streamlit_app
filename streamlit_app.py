80c492cf64d2173b68221b5a9689c809568a1763
Francisco Zuniga <baycfran42@gmail.com>
2022-04-29 14:26:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
+import pandas
 import streamlit
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
@@ -6,3 +9,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.dataframe(my_fruit_list)
